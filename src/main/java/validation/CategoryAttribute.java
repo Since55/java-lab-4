53fd3d65fac9054ed6c05e7828a11758c0c274d1
Ruslan Ludchak <heckfyua@gmail.com>
2021-12-11 15:18:55 +0200

    lab 4

---
+++ src/main/java/validation/CategoryAttribute.java
@@ -0,0 +1,22 @@
+package validation;
+
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.*;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Constraint(validatedBy = CategoryValidator.class)
+public @interface CategoryAttribute {
+
+    Class<? extends Enum<?>> enumClazz();
+    String message() default "Invalid Input";
+
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+}
